b5243080098ece48b985d55330193a44df227cd6
Aryan Chavan <94561506+ary2708@users.noreply.github.com>
2022-07-24 20:37:42 +0530

    Solved the error and made necessary changes
    1. Changed all the functions in contract 'SafeMath' from 'public' to 'internal'.
    2.  For the functions in the contract 'ERC20Interface' changed 'public' to 'external' and 'constant' to 'view' for decreasing gas fees, improving performance, and also, as the 'constant' keyword is outdated to use in Solidity.
    3. Changed 'YOUR_METAMASK_WALLET_ADDRESS' to 'msg. sender' wherever present.
    4.  Added necessary checks to the 'transfer' and 'transferFrom' functions in the contract.

--- erc20.sol
+++ erc20.sol
@@ -4,22 +4,22 @@ pragma solidity ^0.4.24;
  
 contract SafeMath {
  
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
+    function safeAdd(uint a, uint b) internal pure returns (uint c) {
         c = a + b;
         require(c >= a);
     }
  
-    function safeSub(uint a, uint b) public pure returns (uint c) {
+    function safeSub(uint a, uint b) internal pure returns (uint c) {
         require(b <= a);
         c = a - b;
     }
  
-    function safeMul(uint a, uint b) public pure returns (uint c) {
+    function safeMul(uint a, uint b) internal pure returns (uint c) {
         c = a * b;
         require(a == 0 || c / a == b);
     }
  
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
+    function safeDiv(uint a, uint b) internal pure returns (uint c) {
         require(b > 0);
         c = a / b;
     }
@@ -29,12 +29,12 @@ contract SafeMath {
 //ERC Token Standard #20 Interface
  
 contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
  
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -53,7 +53,7 @@ contract QKCToken is ERC20Interface, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
-    uint public _totalSupply;
+    uint public  _totalSupply;
  
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
@@ -63,32 +63,35 @@ contract QKCToken is ERC20Interface, SafeMath {
         name = "QuikNode Coin";
         decimals = 2;
         _totalSupply = 100000;
-        balances[YOUR_METAMASK_WALLET_ADDRESS] = _totalSupply;
-        emit Transfer(address(0), YOUR_METAMASK_WALLET_ADDRESS, _totalSupply);
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
  
-    function totalSupply() public constant returns (uint) {
+    function totalSupply() external view returns (uint) {
         return _totalSupply  - balances[address(0)];
     }
  
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+    function balanceOf(address tokenOwner) external view returns (uint balance) {
         return balances[tokenOwner];
     }
  
-    function transfer(address to, uint tokens) public returns (bool success) {
+    function transfer(address to, uint tokens) external returns (bool success) {
+        require(balances[msg.sender]>=tokens);
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
  
-    function approve(address spender, uint tokens) public returns (bool success) {
+    function approve(address spender, uint tokens) external returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
  
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+    function transferFrom(address from, address to, uint tokens) external returns (bool success) {
+         require(allowed[from][msg.sender]>=tokens);
+         require(balances[from]>=tokens);
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
@@ -96,7 +99,7 @@ contract QKCToken is ERC20Interface, SafeMath {
         return true;
     }
  
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+    function allowance(address tokenOwner, address spender) external constant returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
  
